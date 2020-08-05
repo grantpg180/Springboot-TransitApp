705056f75226000bd3070295c9850f21036bf31a
grantpg180 <62108947+grantpg180@users.noreply.github.com>
2020-08-05 00:20:44 -0400

    changed map icon to a Bus image, attempted to add an Info Window to display bus Number

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -10,12 +10,29 @@ function initMap() {
     });
 	
     for (i=0; i<busLocations.length; i++){
+        var image = 'https://maps.google.com/mapfiles/kml/shapes/bus.png'; //adds a Bus icon
         var marker = new google.maps.Marker({
             position: { 
                 lat: parseFloat(busLocations[i].LATITUDE), 
                 lng: parseFloat(busLocations[i].LONGITUDE) 
             },
             map: map,
+            icon: image //sets the Bus icon
         });
+
+        //Add info window
+    /*let contentString = 
+    '<div id="content">' + '</div>' +
+    '<h3 id = "busNumber" class="busNumber">Bus Number</h3>' +
+    '<div id="bodyContent">'+
+    '<p></p>' + '</div>';*/
+    /*let infoWindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    image.addListener('click', function() {
+        infoWindow.open(map, marker);
+    });*/
+
     }
 }
